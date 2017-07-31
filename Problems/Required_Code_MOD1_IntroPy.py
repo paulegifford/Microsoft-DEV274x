55e6622275a08042e83391f5c1669942aa39c54c
Siddharth Kumaran <siddgood@gmail.com>
2017-07-31 16:34:58 -0500

    Create Required_Code_MOD1_IntroPy.py

---
+++ Problems/Required_Code_MOD1_IntroPy.py
@@ -0,0 +1,6 @@
+def words():
+    quote = input("Enter a 1 sentence quote, non-alpha separate words: ")
+    word = ""
+    for ltr in quote:
+        if ltr.isalpha():
+            word = word + ltr
